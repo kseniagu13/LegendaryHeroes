6aeccbd51857da5852882a46b95dd696b1f2a780
Ksenia Gustsenko <kseniagustsenko@Ksenias-MacBook-Air.local>
2019-12-16 14:01:05 +0200

    chuck norris

---
+++ MyFirstGame/Player.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace MyFirstGame
+{
+    class Player
+    {
+        string name;
+        int healthPoint;
+
+        public Player (string _name)
+        {
+            Random rnd = new Random();
+            name = _name;
+            healthPoint = rnd.Next(100,151) ;
+        }
+
+        public string Name
+        {
+            get { return name; }
+        }
+
+        public int HealthPoint
+        {
+            get { return healthPoint; }
+        }
+
+        public int MakeDamage()
+        {
+            Random rnd = new Random();
+            return rnd.Next(10, 51); //lihtsalt returnib numbri  
+        }
+        public  void TakeDamage(int damage) //ei returni numbri vaid arvutiba healtpointidest maha
+        {
+            if (healthPoint <= 0) //kontroll, et healthpoint ei saa väiksem kui 0 olla.
+            {
+                healthPoint = 0;
+            }
+            else
+            {
+                healthPoint -= damage;
+            }
+        }
+
+    }
+}
---
+++ MyFirstGame/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace MyFirstGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Player Arnold = new Player("Arnold Schwarzenegger");
+            Player Chuck = new Player("Chuck Norris");
+
+            for (int i=0; i<3; i++)
+            {
+                Arnold.TakeDamage(Chuck.MakeDamage());
+                Console.WriteLine(Arnold.HealthPoint);
+                Chuck.TakeDamage(Arnold.MakeDamage());
+                Console.WriteLine(Chuck.HealthPoint);
+
+            }
+
+            if (Arnold.HealthPoint>Chuck.HealthPoint)
+            {
+                Console.WriteLine($"Arnold you are winner! He has {Arnold.HealthPoint} points.");
+            }
+            else if (Chuck.HealthPoint>Arnold.HealthPoint)
+            {
+                Console.WriteLine($"Chuck you are winner! You have {Chuck.HealthPoint} points.");
+            }
+            else
+            {
+                Console.WriteLine("Its a tie");
+            }
+
+        }
+    }
+}
+
